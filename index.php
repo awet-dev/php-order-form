557cd0bd81cadd8fcef68b9accdf59936f501aa0
awet tesfay <whitezscope@gmail.com>
2020-10-06 10:58:48 +0200

    switch b/n the food and drink links

--- index.php
+++ index.php
@@ -26,14 +26,12 @@ if (!isset($_SESSION["zipcode"])) {
 }
 
 function whatIsHappening() {
-    /*
      echo '<h2>$_GET</h2>';
     var_dump($_GET);
     echo '<h2>$_POST</h2>';
     var_dump($_POST);
     echo '<h2>$_COOKIE</h2>';
     var_dump($_COOKIE);
-     */
     echo '<h2>$_SESSION</h2>';
     var_dump($_SESSION);
 }
@@ -47,13 +45,24 @@ $products = [
     ['name' => 'Club Salmon', 'price' => 5]
 ];
 
-$products = [
-    ['name' => 'Cola', 'price' => 2],
-    ['name' => 'Fanta', 'price' => 2],
-    ['name' => 'Sprite', 'price' => 2],
-    ['name' => 'Ice-tea', 'price' => 3],
-];
-
+if(isset($_GET['food'])) {
+    if ($_GET['food'] == 1) {
+        $products = [
+            ['name' => 'Club Ham', 'price' => 3.20],
+            ['name' => 'Club Cheese', 'price' => 3],
+            ['name' => 'Club Cheese & Ham', 'price' => 4],
+            ['name' => 'Club Chicken', 'price' => 4],
+            ['name' => 'Club Salmon', 'price' => 5]
+        ];
+    } else if ($_GET['food'] == 0) {
+        $products = [
+            ['name' => 'Cola', 'price' => 2],
+            ['name' => 'Fanta', 'price' => 2],
+            ['name' => 'Sprite', 'price' => 2],
+            ['name' => 'Ice-tea', 'price' => 3],
+        ];
+    }
+}
 $totalValue = 0;
 
 $success_order = "Fill the form to order your food?";
@@ -121,6 +130,4 @@ function test_input($data) {
     return $data;
 }
 
-
-whatIsHappening();
 require 'form-view.php';
